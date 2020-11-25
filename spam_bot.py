526ebf88663d437ffbcdd531a7c8e95a070eb1d8
Shubham Sahu <67262119+shubham1208@users.noreply.github.com>
2020-11-26 00:52:04 +0530

    Update spam_bot.py

--- spam_bot.py
+++ spam_bot.py
@@ -1,13 +1,3 @@
 # spam_bot
-# A simple code to spam the same message over and over.
 
-import pyautogui
-import time
-n = 0
-time.sleep(3)
-# while loop to set the number of messages sent 
-while n<=50:
-    n +=1
-    # this text will be spammed over and over
-    pyautogui.typewrite("We are monkey!")
-    pyautogui.press("enter")
+A simple code to spam the same message over and over.
